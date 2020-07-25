7d8741a5474ae633e3edd7e1419e4b9296a92ce7
anagarg <64941411+anagarg@users.noreply.github.com>
2020-07-24 18:14:59 -0700

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -1,27 +1,24 @@
-class Paper{
-    constructor(x, y, diameter) {
-        var options = {
-            isStatic:false,
-            restitution:0.3,
-            friction:0.5,
-            density:1.2
-        }
-      this.body = Matter.Bodies.circle(200,200,140,options);
-      World.add(world, this.body);
-      }
-
-      display(){
-        var pos =this.body.position;
-        var angle = this.body.angle;
-        console.log("hello");
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        //ellipseMode(CENTER);
-        fill(100);
-        ellipse(200,200,70,70);
-        pop();
-      }
-
-         
-}
+class Paper 
+  { constructor(x,y,r) {
+     var options= {
+        //isStatic: false, 
+        restitution:0.3, 
+        friction:0.5, 
+        density:1.2 
+      } 
+    this.x=x; this.y=y; 
+    this.r = r
+    this.body=Bodies.circle(this.x, this.y, this.r/2, options) 
+    World.add(world, this.body); 
+  } 
+  
+  display() { 
+    var pos =this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    rectMode(CENTER) 
+    strokeWeight(3); 
+    fill(255,0,255) 
+    ellipse(0,0,this.r, this.r); 
+    pop() } 
+  }
--- sketch.js
+++ sketch.js
@@ -1,10 +1,18 @@
-var paper, ground
+var ground;
+var paper;
+var launcher;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
+const Constraint = Matter.Constraint;
 var box1, box2, box3;
+var ballImg, dustbinImg;
+
+function preload() {
+	dustbinImg = loadImage('Dustbin.png');
+}
 
 function setup() {
 	createCanvas(800, 700);
@@ -14,40 +22,40 @@ function setup() {
 
 	ground = new Ground(400,680,800,40);
 
-	paper = new Paper(200,200,100);
+	paper = new Paper(200,150,40);
 
 	Engine.run(engine);
 
-	box1=createSprite(600,650,200,20);
-	box1.shapeColor=color(255,0,0)
-	box1.isStatic = true;
+	box1=new Ground(600,650,220,20);
+	box2=new Ground(480,500,20,300);
+	box3=new Ground(680,500,20,300);
 
-	box2=createSprite(500,610,20,100);
-	box2.shapeColor=color(255,0,0)
-	box2.isStatic = true;
-
-	box3=createSprite(700,610,20,100);
-	box3.shapeColor=color(255,0,0)
-	box3.isStatic = true;  
+	launcher = new Launcher(paper.body,{x:200, y:150});
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  
-  drawSprites();
- 
-  paper.display();
-  ground.display();
+
   box1.display();
   box2.display();
   box3.display();
+  image(dustbinImg,470,325);
+  paper.display();
+  ground.display();
+  launcher.display();
 }
 
-keyPressed() 
-        if(keyCode === UP_ARROW) {
-          Matter.body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
-        }
-	  
-	
+function mouseDragged() {
+    Matter.Body.setPosition(paper.body, {x: mouseX, y: mouseY})
+}
+function mouseReleased() {
+    launcher.fly()
+}
+
+//function keyPressed() { 
+//	if (keyCode === UP_ARROW) {
+//		 Matter.body.applyForce(paper.body, paper.body.position, {x: 85, y: -85}); 
+//	} 
+//}
